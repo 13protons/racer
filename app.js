6c877769591d6d55d1685e1a81a3204c712fbe79
Alan Languirand <alan@13protons.com>
2016-07-17 12:14:28 -0400

    config

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(process.env.port || 3000);
+server.listen(process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
